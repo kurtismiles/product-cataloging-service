915de3a1936142072e0ae610cb71dbab222432e1
Kurtis Miles <kjm6121@psu.edu>
2022-02-23 14:09:17 -0600

    Initial commit

---
+++ test/product-cataloging-service.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ProductCatalogingService = require('../lib/product-cataloging-service-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/product-cataloging-service-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ProductCatalogingService.ProductCatalogingServiceStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
